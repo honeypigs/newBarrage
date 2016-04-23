fdce8803b2fec29f41e7b4e7cd13426ba6a554b2
honeypigs <1002213567@qq.com>
2016-04-23 16:21:47 +0800

    barrage

---
+++ public/js/data.js
@@ -0,0 +1,78 @@
+	 barrage_content = [
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "pink"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ajajajajajajajfiasjdofjs",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "doqif-84v13th9fdsdwfp",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "<dnsvnsadfniomd></dnsvnsadfniomd>",
+			"size" : 20 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 24 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "pink"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "white"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "yellow"
+		},
+		{
+			"content" : "ahahahahahah",
+			"size" : 16 ,
+			"color" : "red"
+		},
+
+	]
---
+++ public/js/getData.js
@@ -0,0 +1,10 @@
+var getData = function () {
+	$.ajax({
+		type : "GET",
+		url : "public/js/data.json",
+		dataType : "json",
+		success : function() {
+			console.log(1);
+		}																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																		
+	})
+};
